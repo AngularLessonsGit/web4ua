f884403ece5a5badb0d03d44e84076ecb67eae7f
Mykyta Paletskykh <mykyta.paletskykh@perfectial.com>
2020-07-22 17:53:13 +0300

    Added client

--- app.js
+++ app.js
@@ -1,47 +1,20 @@
 const express = require('express');
 const app = express();
 
+app.use(express.static(`${__dirname}/client/dist/public`));
+
 //MongoDb
-const mongo = require('mongoose');
-mongo.connect(`mongodb+srv://${process.env.userName}:${process.env.userPwd}@web4ua.966wc.mongodb.net/${process.env.dbName}?retryWrites=true&w=majority`, {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-})
+const {connect, Users} = require('./mongodb');
+connect()
     .then(() => { console.log('MongoDb connected') })
     .catch((err) => { console.log(err) });
 
-const UsersSchema = new mongo.Schema({
-    name: {
-        type: String,
-        required: true,
-    },
-    password: {
-        type: String,
-        required: true,
-    }
-});
-
-const Users = mongo.model('users', UsersSchema);
-
 //API
-app.get('/', (req, res) => {
-    Users.find()
-    .then((users)=> {
-        if (users.length == 0) {
-            Users.create({
-                name: 'test01',
-                password: 'pass01',
-            })
-            .then((user) => res.send(user))
-            .catch((err) => res.send(err));
-        } else {
-            res.send(users);
-        }
-    })
-    .catch((err) => res.send(err));
+app.get('*', function(req, res) {
+    res.sendFile(`${__dirname}/client/dist/public/index.html`);
 });
 
 const port = process.env.port || 8000;
 app.listen(port, () => {
-    console.log('Let1s go!');
+    console.log(`Express server listening on port ${port}`);
 });
